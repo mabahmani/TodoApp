247f10a0b690a6cb26e53a1532bede92b42394d4
amin <aminbahmani10@gmail.com>
2019-09-22 21:15:11 +0330

    add no internet layout

--- app/src/main/java/com/example/todo/splash/SplashActivity.java
+++ app/src/main/java/com/example/todo/splash/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.example.todo.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.core.app.DialogCompat;
 
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import com.example.todo.R;
 
@@ -12,5 +16,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if (!isNetworkConnected()){
+            AppCompatDialog dialog = new AppCompatDialog(SplashActivity.this);
+            dialog.setContentView(LayoutInflater.from(this).inflate(R.layout.dialoog_no_internet,null,false));
+            dialog.show();
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(SplashActivity.CONNECTIVITY_SERVICE);
+        return (cm != null ? cm.getActiveNetworkInfo() : null) != null && cm.getActiveNetworkInfo().isConnected();
     }
 }
