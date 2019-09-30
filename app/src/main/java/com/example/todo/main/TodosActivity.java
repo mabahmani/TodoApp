524686ec5a3939f37a9f1121e4a2bb87e41eab18
amin <aminbahmani10@gmail.com>
2019-09-30 23:13:15 +0330

    add title and fresh start anim with font.

--- app/src/main/java/com/example/todo/main/TodosActivity.java
+++ app/src/main/java/com/example/todo/main/TodosActivity.java
@@ -2,9 +2,13 @@ package com.example.todo.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.todo.R;
+
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class TodosActivity extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class TodosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 }
