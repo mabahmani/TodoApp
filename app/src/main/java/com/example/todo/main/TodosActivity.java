255c83820ecb891ce3cda984f1a809e138c9eee5
amin <aminbahmani10@gmail.com>
2019-10-01 19:47:18 +0330

    add options menu

--- app/src/main/java/com/example/todo/main/TodosActivity.java
+++ app/src/main/java/com/example/todo/main/TodosActivity.java
@@ -4,23 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.todo.R;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class TodosActivity extends AppCompatActivity {
 
+    @BindView(R.id.bottom_app_bar)
+    BottomAppBar bottomAppBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todos);
+        ButterKnife.bind(this);
+        setSupportActionBar(bottomAppBar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.todos_option_menu,menu);
+        return true;
     }
 
     @Override
