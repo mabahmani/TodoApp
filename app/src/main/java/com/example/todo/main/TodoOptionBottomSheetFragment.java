109a4d4f312f91dbd7ab4d7de9eca65f11d947cc
amin <aminbahmani10@gmail.com>
2019-10-02 21:29:35 +0330

    add options menu bottom sheet

---
+++ app/src/main/java/com/example/todo/main/TodoOptionBottomSheetFragment.java
@@ -0,0 +1,30 @@
+package com.example.todo.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.todo.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class TodoOptionBottomSheetFragment extends BottomSheetDialogFragment {
+
+    public TodoOptionBottomSheetFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bottom_sheet_todo_option,container,false);
+    }
+}
--- app/src/main/java/com/example/todo/main/TodosActivity.java
+++ app/src/main/java/com/example/todo/main/TodosActivity.java
@@ -1,5 +1,6 @@
 package com.example.todo.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,9 +8,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.todo.R;
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +35,17 @@ public class TodosActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.todos_option_menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.options) {
+            TodoOptionBottomSheetFragment bottomSheetFragment = new TodoOptionBottomSheetFragment();
+            bottomSheetFragment.show(getSupportFragmentManager(), null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
